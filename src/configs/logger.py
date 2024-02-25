6172771d540b9987fc0c1ee44215e98ff85bf893
Syrnnik <sakh.alexandr@gmail.com>
2024-02-25 20:02:25 +0300

    fix: logger

--- src/configs/logger.py
+++ src/configs/logger.py
@@ -9,55 +9,41 @@ from services.sentry.init_sentry import init_sentry
 
 init_sentry()
 
-ERROR_MESSAGE_FORMAT = "[%(asctime)s] ERROR - %(levelname)s: %(message)s"
-INFO_MESSAGE_FORMAT = "[%(asctime)s] INFO - %(message)s"
+MESSAGE_FORMAT = "[%(asctime)s] %(levelname)s - %(message)s"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=MESSAGE_FORMAT
+)
 
 
 def catch_error(
     tag: LogTag,
     error: Exception,
     project_id: str | None = None,
-    user_email: str | None = None
 ):
-    # DO NOT MOVE THIS IMPORT unless error :)
-    # TODO: write what error raises if import not here but in top of this file
-    from services.firebase.firestore.update_project import update_project_status_and_translated_link_by_id
-
-    logging.basicConfig(
-        level=logging.ERROR,
-        format=ERROR_MESSAGE_FORMAT
-    )
     logging.error(msg=f"({tag.value}) {str(error)}")
 
-    if IS_DEV_ENVIRONMENT:
-        return
+    if not IS_DEV_ENVIRONMENT:
+        # Send error to Sentry
+        sentry_sdk.capture_exception(error)
 
-    # Send error to Sentry
-    sentry_sdk.capture_exception(error)
+        # DO NOT MOVE THIS IMPORT unless error :)
+        # TODO: write what error raises if import not here but in top of this file
+        from services.firebase.firestore.update_project import update_project_status_and_translated_link_by_id
 
-    # Update project status to 'translationError'
-    if project_id is not None:
-        update_project_status_and_translated_link_by_id(
-            project_id=project_id,
-            status=ProjectStatus.TRANSLATION_ERROR.value,
-            translated_file_link=""
-        )
-        # # Send email to user about project error
-        # if user_email is not None:
-        #     send_email_with_api(
-        #         user_email=user_email,
-        #         email_template=EmailTemplate.ProjectError
-        #     )
-
+        # Update project status to 'translationError'
+        if project_id is not None:
+            update_project_status_and_translated_link_by_id(
+                project_id=project_id,
+                status=ProjectStatus.TRANSLATION_ERROR.value,
+                translated_file_link=""
+            )
     raise error
 
 
 def print_info_log(tag: LogTag, message: str):
-    logging.basicConfig(
-        level=logging.INFO,
-        format=INFO_MESSAGE_FORMAT,
-    )
-    logging.info(msg=f"({tag.value}) {message}")
+    logging.info(msg=f"({tag}) {message}")
 
 
 if __name__ == "__main__":
