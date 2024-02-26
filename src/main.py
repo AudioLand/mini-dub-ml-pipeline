b7dd7ccdcf71be56cf6e5a0fff106d5d7dd73cad
Syrnnik <sakh.alexandr@gmail.com>
2024-02-26 17:25:04 +0300

    fix: CORS

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from controllers.generate import dub_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 app.include_router(dub_router)
 
